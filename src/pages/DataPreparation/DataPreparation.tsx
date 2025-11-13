3e6af1f876ff9d562f8ec4ae39172558afc14834
Aneela Ammu Thomas <aneela.thomas@alshaya.com>
2025-11-13 22:19:45 +0530

    changes

---
+++ src/pages/DataPreparation/DataPreparation.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './DataPreparation.module.scss';
+
+const DataPreparation: React.FC = () => {
+
+    return (
+        <div className={styles.dataPreparationContainer}>
+            {/* Add your content here */}
+            DataPreparation
+            <p>This is the DataPreparation Page content.</p>
+        </div>
+    );
+};
+
+export default DataPreparation;
--- src/pages/SamplePage/SamplePage.tsx
+++ src/pages/SamplePage/SamplePage.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './SamplePage.module.scss';
 
 const SamplePage: React.FC = () => {
 
     return (
-        <div>
+        <div className={styles.container}>
             {/* Add your content here */}
             SamplePage
             <p>This is the Sample Page content.</p>
